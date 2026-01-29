2e97ba959aaed17b33ad79b141a8c132d3aa10f2
ge-xiaoxiao <marshall_gefxiang@163.com>
2026-01-29 20:18:56 +0800

    update config

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/llmpedia/",
+  base: "/",
 
   lang: "zh-CN",
   title: "LLMPedia - 大模型面试八股题",
